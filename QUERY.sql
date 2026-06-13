f1cb86df1ae9eff214d585dfde08a133f07195b9
kira217-cyber <gamingkira217@gmail.com>
2026-06-14 00:35:09 +0600

    solve query 5 relative all users using left join

--- QUERY.sql
+++ QUERY.sql
@@ -210,3 +210,17 @@ FROM
     Bookings b
     INNER JOIN Users u ON b.user_id = u.user_id
     INNER JOIN Matches m ON b.match_id = m.match_id;
+
+-- Query 5: Retrieve all users and their booking IDs, including users with no bookings
+SELECT
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM
+    Users u
+    LEFT JOIN Bookings b ON u.user_id = b.user_id
+ORDER BY
+    u.user_id,
+    b.booking_id;
+
+
